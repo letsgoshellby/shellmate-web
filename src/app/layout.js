bf6a80d5f41c15131cc737216acd780fe9924f4f
inqueue0979 <inqueue0979@gmail.com>
2025-09-19 06:02:02 +0900

    Feat : 없는 임포트 추가 및 폰트 변경 (Pretendard)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "셸메이트 - 느린학습자 상담 플랫폼",
@@ -21,9 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="stylesheet" as="style" crossOrigin="" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" />
+      </head>
+      <body className="antialiased font-sans">
         <AuthProvider>
           {children}
           <Toaster position="top-right" />
