a0f26127e0b01a32da4576afeb8cd3d6cd1f4268
jhj04 <hj650272@gmail.com>
2026-02-14 03:07:16 +0900

    [FIX] : 포트원 결제 오류 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script";
+import { ScriptLoader } from "@/components/ScriptLoader";
 
 export const metadata = {
   title: "셸메이트 - 느린학습자 상담 플랫폼",
@@ -15,18 +15,7 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" as="style" crossOrigin="" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" />
       </head>
       <body className="antialiased font-sans">
-        {/* PortOne V2 결제 SDK */}
-        <Script
-          src="https://cdn.portone.io/v2/browser-sdk.js"
-          strategy="beforeInteractive"
-        />
-        {/* Kakao JavaScript SDK */}
-        <Script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-          integrity="sha384-TiCUE00h+afO/VrfOWoJdGLJhYIEBKzq+PtTbY0BELMRyDUTdLpJ1cC9F8s3h0wn"
-          crossOrigin="anonymous"
-          strategy="beforeInteractive"
-        />
+        <ScriptLoader />
         <AuthProvider>
           {children}
           <Toaster position="top-right" />
