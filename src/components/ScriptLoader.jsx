9fd2bbd9d446c0615d9b59d591af6ed9956f337c
jhj04 <hj650272@gmail.com>
2026-02-14 03:20:49 +0900

    [FEAT] : 포트원, 카카오로그인 loader

---
+++ src/components/ScriptLoader.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Script from 'next/script';
+
+export function ScriptLoader() {
+  return (
+    <>
+      {/* PortOne V2 결제 SDK */}
+      <Script
+        src="https://cdn.portone.io/v2/browser-sdk.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log('PortOne SDK loaded');
+        }}
+      />
+
+      {/* Kakao JavaScript SDK */}
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log('Kakao SDK loaded successfully', !!window.Kakao);
+        }}
+        onError={(e) => {
+          console.error('Kakao SDK failed to load', e);
+        }}
+      />
+    </>
+  );
+}
