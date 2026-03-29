27b6fd0b93b3884b3b9bf3c626d0fa3c092eb890
jhj04 <hj650272@gmail.com>
2026-03-30 02:29:09 +0900

    FEAT : 301 리다이렉트 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'shellmate-web-theta.vercel.app',
+          },
+        ],
+        destination: 'https://shellmate.letsgoshellby.com/:path*',
+        permanent: true, // 301 리다이렉트
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.shellmate.letsgoshellby.com',
+          },
+        ],
+        destination: 'https://shellmate.letsgoshellby.com/:path*',
+        permanent: true, // 301 리다이렉트
+      },
+    ];
+  },
+};
 
 export default nextConfig;
